2cd06115648e34353912523e24aebc0b96c02afb
Murphy Trueman <hello@murphytrueman.com>
2021-01-24 14:46:02 +1100

    Added stamp functionality

---
+++ src/js/stamps.js
@@ -0,0 +1,29 @@
+let number = 0
+const stamps = [
+  "../img/circles.svg",
+  "../img/heart.svg",
+  "../img/moon.svg",
+  "../img/rainbow.svg",
+  "../img/shooting-star.svg",
+  "../img/waves.svg"
+]
+
+const stampsTag = document.querySelector("div.stamps")
+
+const addStamp = function(x, y) {
+  const img = document.createElement('img')
+  img.setAttribute('src', stamps[number])
+  img.style.left = x + 'px'
+  img.style.top = y + 'px'
+
+  stampsTag.appendChild(img)
+
+  number = number + 1
+  if(number > stamps.length -1) {
+    number = 0
+  }
+}
+
+document.addEventListener('click', function(event) {
+  addStamp(event.pageX, event.pageY)
+})
